d04a455665eb5af60d222fb06e1b429872256dde
Juwon Lee <good9797@outlook.com>
2018-02-24 23:41:40 +0900

    20180224 Pixel-City 진행중2

--- pixel-city/pixel-city/Controller/MapVC.swift
+++ pixel-city/pixel-city/Controller/MapVC.swift
@@ -10,26 +10,51 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapVC: UIViewController {
+class MapVC: UIViewController, UIGestureRecognizerDelegate {
     @IBOutlet weak var mapView: MKMapView!
     
     var locationManager = CLLocationManager()
     let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000 // 1000미터라는 뜻인가?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
         locationManager.delegate = self
+        configureLocationServices()
+        addDoubleTap()
+    }
+    
+    func addDoubleTap() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(dropPin(sender:)))
+        // 더블탭 적용
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.delegate = self
+        mapView.addGestureRecognizer(doubleTap)
     }
     
     @IBAction func centerMapBtnWasPressed(_ sender: Any) {
-        
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
     }
-    
 }
 
 extension MapVC: MKMapViewDelegate {
+    // 지도를 현재 위치로 놓기 및 지도 크기 설정
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        // 아래 주석은 실패한 코드
+        // let coordinateRegion = MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan.init(latitudeDelta: regionRadius, longitudeDelta: regionRadius))
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
     
+    @objc func dropPin(sender: UITapGestureRecognizer) {
+        // Drop the pin on the map.
+        let touchPoint = sender.location(in: mapView)
+        print(touchPoint)
+    }
 }
 
 extension MapVC: CLLocationManagerDelegate {
@@ -39,5 +64,9 @@ extension MapVC: CLLocationManagerDelegate {
         } else {
             return
         }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
     }
 }
