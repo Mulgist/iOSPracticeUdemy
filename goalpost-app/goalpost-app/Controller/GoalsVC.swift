084a4ee7ace9318fe8632238d05bf2e76b0920d0
Juwon Lee <good9797@outlook.com>
2018-02-27 00:00:49 +0900

    20180226 Goalpost-App 진행중

---
+++ goalpost-app/goalpost-app/Controller/GoalsVC.swift
@@ -0,0 +1,23 @@
+//
+//  GoalsVC.swift
+//  goalpost-app
+//
+//  Created by 이주원 on 2018. 2. 26..
+//  Copyright © 2018년 이주원. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class GoalsVC: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func addGoalBtnWasPressed(_ sender: Any) {
+        
+    }
+}
---
+++ goalpost-app/goalpost-app/View/GoalCell.swift
@@ -0,0 +1,21 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by 이주원 on 2018. 2. 26..
+//  Copyright © 2018년 이주원. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+}
